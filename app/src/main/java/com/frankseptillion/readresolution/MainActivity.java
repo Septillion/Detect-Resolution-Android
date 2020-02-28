e6c841b5ecd93023c101e4a4160894e71b0641b7
septillion <frankseptillion@msn.cn>
2020-02-28 21:49:59 +0800

    Model number is now easier to understand.

--- app/src/main/java/com/frankseptillion/readresolution/MainActivity.java
+++ app/src/main/java/com/frankseptillion/readresolution/MainActivity.java
@@ -1,17 +1,19 @@
 package com.frankseptillion.readresolution;
 
-import android.graphics.Point;
+import android.bluetooth.BluetoothAdapter;
 import android.os.Build;
-import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.Display;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
+//import com.jaredrummler.android.device.DeviceName;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView model;
+    private String modelNumber2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,43 +21,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Initializing View
-        TextView model = findViewById(R.id.idModel);
+        model = findViewById(R.id.idModel);
         TextView resolution = findViewById(R.id.idResolution);
         TextView dementions = findViewById(R.id.idDemensions);
         TextView density = findViewById(R.id.idDesity);
         TextView isHDR = findViewById(R.id.idIsHDR);
-        //TextView hdrcap = findViewById(R.id.idHDRcap);
 
 
         // Get Display Information
         Display display = getWindowManager().getDefaultDisplay();
         DisplayMetrics dm = new DisplayMetrics();
         display.getRealMetrics(dm);
-        //int[] hdrCapEnum = display.getHdrCapabilities().getSupportedHdrTypes();
-        //String hdrCapabilities = new String();
         String isHdr = new String();
-        String modelNumber = Build.MODEL;
+        //String modelNumber = Build.MODEL;
 
+        //String modelNumber1 = DeviceName.getDeviceName();
 
-        /*
-        for (int i:hdrCapEnum
-             ) {
-            switch (i) {
-                case 1:
-                    hdrCapabilities = hdrCapabilities + "Dolby Vision ";
-                    break;
-                case 2:
-                    hdrCapabilities = hdrCapabilities + "HDR10 ";
-                    break;
-                case 3:
-                    hdrCapabilities = hdrCapabilities + "HDR10+ ";
-                    break;
-                case 4:
-                    hdrCapabilities = hdrCapabilities + "HLG ";
-                    break;
-            }
+        if(BluetoothAdapter.getDefaultAdapter().getName()!=null){
+            modelNumber2 = BluetoothAdapter.getDefaultAdapter().getName();
+        }else{
+            modelNumber2 = Build.MODEL;
         }
-        */
+
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             if (display.isHdr()){
@@ -69,12 +56,11 @@ public class MainActivity extends AppCompatActivity {
 
 
         // Set Display Information
-        model.setText(modelNumber);
+        model.setText(modelNumber2);
         resolution.setText(dm.widthPixels + " x " + dm.heightPixels);
         dementions.setText((int)Math.ceil(dm.widthPixels/dm.density) + " x " + (int)Math.ceil(dm.heightPixels/dm.density));
         density.setText(Integer.toString(dm.densityDpi));
         isHDR.setText(isHdr);
-        //hdrcap.setText(hdrCapabilities);
     }
 
 }
