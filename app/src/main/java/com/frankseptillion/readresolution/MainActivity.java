aab3ef4e91c0b96568266d79ad98112a148c3c07
septillion <frankseptillion@msn.cn>
2020-03-06 12:36:27 +0800

    added wide color detection

--- app/src/main/java/com/frankseptillion/readresolution/MainActivity.java
+++ app/src/main/java/com/frankseptillion/readresolution/MainActivity.java
@@ -11,18 +11,28 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView model, resolution, dimensions, density, isHDR, isWideColor, refreshRate;
+    private String modelNumber2, StringIsWideColor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // Initializing View
-        TextView model = findViewById(R.id.idModel);
-        TextView resolution = findViewById(R.id.idResolution);
-        TextView dimensions = findViewById(R.id.idDemensions);
-        TextView density = findViewById(R.id.idDesity);
-        TextView isHDR = findViewById(R.id.idIsHDR);
-        TextView refreshRate = findViewById(R.id.refreshRate);
+        model = findViewById(R.id.idModel);
+        resolution = findViewById(R.id.idResolution);
+        dimensions = findViewById(R.id.idDemensions);
+        density = findViewById(R.id.idDesity);
+        isHDR = findViewById(R.id.idIsHDR);
+        isWideColor = findViewById(R.id.isWideColor);
+        refreshRate = findViewById(R.id.refreshRate);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
 
 
         // Get Display Information
@@ -32,77 +42,74 @@ public class MainActivity extends AppCompatActivity {
         StringBuilder isHdr = new StringBuilder("");
 
         // Get Device Name
-        String modelNumber2;
-        if(BluetoothAdapter.getDefaultAdapter().getName()!=null){
-            modelNumber2 = BluetoothAdapter.getDefaultAdapter().getName();
-        }else{
+        try {
+            if(BluetoothAdapter.getDefaultAdapter().getName()!=null){
+                modelNumber2 = BluetoothAdapter.getDefaultAdapter().getName();
+            }else{
+                modelNumber2 = Build.MODEL;
+            }
+        }catch (Exception e){
             modelNumber2 = Build.MODEL;
         }
 
-
-        // Get isHDR
-        /*
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            if (display.isHdr()){
-                isHdr = "Yes";
-            }else{
-                isHdr = "No";
-            }
-        }else {
-            isHdr = "No";
-        }
-        */
-
         // Get HDR Capabilities
-            int[] HDRcapabilities = display.getHdrCapabilities().getSupportedHdrTypes();
-            if (HDRcapabilities.length > 0) {
-                StringBuilder HDRenum = new StringBuilder();
-                int HDR10flag = 0;
-                for (int i : HDRcapabilities) {
-                    switch (i) {
-                        case Display.HdrCapabilities.HDR_TYPE_HLG:
-                            HDRenum.append("HLG, ");
-                            break;
-                        case Display.HdrCapabilities.HDR_TYPE_HDR10:
-                            HDR10flag = 1;
-                            break;
-                        case 4:
-                            HDR10flag = 2;
-                            break;
-                        case Display.HdrCapabilities.HDR_TYPE_DOLBY_VISION:
-                            HDR10flag = 3;
-                            break;
-                        default:
-                            HDRenum = new StringBuilder("No");
-                    }
-                }
-                switch (HDR10flag){
-                    case 1:
-                        HDRenum.append("HDR10");
+        int[] HDRcapabilities = display.getHdrCapabilities().getSupportedHdrTypes();
+        if (HDRcapabilities.length > 0) {
+            StringBuilder HDRenum = new StringBuilder();
+            int HDR10flag = 0;
+            for (int i : HDRcapabilities) {
+                switch (i) {
+                    case Display.HdrCapabilities.HDR_TYPE_HLG:
+                        HDRenum.append("HLG, ");
                         break;
-                    case 2:
-                        HDRenum.append("HDR10+");
+                    case Display.HdrCapabilities.HDR_TYPE_HDR10:
+                        HDR10flag = 1;
                         break;
-                    case 3:
-                        HDRenum.append("Dolby Vision");
+                    case 4:
+                        // HDR10+ added in API 29
+                        HDR10flag = 2;
+                        break;
+                    case Display.HdrCapabilities.HDR_TYPE_DOLBY_VISION:
+                        HDR10flag = 3;
                         break;
                     default:
-                        break;
+                        HDRenum = new StringBuilder(getString(R.string.notSupported));
                 }
-                isHdr.append(HDRenum.toString());
-            } else {
-                isHdr.append("No");
             }
+            // Only show highest HDR standard.
+            switch (HDR10flag){
+                case 1:
+                    HDRenum.append("HDR10");
+                    break;
+                case 2:
+                    HDRenum.append("HDR10+");
+                    break;
+                case 3:
+                    HDRenum.append("Dolby Vision");
+                    break;
+                default:
+                    break;
+            }
+            isHdr.append(HDRenum.toString());
+        } else {
+            isHdr.append(getString(R.string.notSupported));
+        }
 
-
+        // Get Wide Color Gamut
+        if (getResources().getConfiguration().isScreenWideColorGamut()){
+            StringIsWideColor = getString(R.string.supported);
+        }else {
+            StringIsWideColor = getString(R.string.notSupported);
+        }
 
         // Set Display Information
         model.setText(modelNumber2);
-        resolution.setText(dm.widthPixels + " x " + dm.heightPixels);
-        dimensions.setText((int)Math.ceil(dm.widthPixels/dm.density) + " x " + (int)Math.ceil(dm.heightPixels/dm.density));
+        resolution.setText(dm.widthPixels + " × " + dm.heightPixels);
+        dimensions.setText((int)Math.ceil(dm.widthPixels/dm.density) + " × " + (int)Math.ceil(dm.heightPixels/dm.density));
         density.setText(dm.density+"x");
         isHDR.setText(isHdr.toString());
+        isWideColor.setText(StringIsWideColor);
         refreshRate.setText((int)display.getRefreshRate()+"Hz");
-    }
 
+    }
 }
